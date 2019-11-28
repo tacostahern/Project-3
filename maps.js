178d31a202b7bf17935017254b4688b0133a7483
tacostahern <35584612+tacostahern@users.noreply.github.com>
2019-11-28 16:01:22 -0600

    added an actual js slider

--- maps.js
+++ maps.js
@@ -190,3 +190,32 @@ function initMap()
   var transitLayer = new google.maps.TransitLayer();
   transitLayer.setMap(map);
 }
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+	var i;
+
+
+	var slides = document.getElementsByClassName("imgfade");
+
+
+	for (i = 0; i < slides.length; i++) {
+
+		slides[i].style.display = "none";
+	}
+
+	slideIndex++;
+
+
+	if (slideIndex > slides.length) {
+		slideIndex = 1;
+	}
+
+
+	slides[slideIndex - 1].style.display = "block";
+
+
+	setTimeout(showSlides, 2000);
+}
