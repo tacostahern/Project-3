faad228c274d106fb1f72b3a87f2e69b45d1fb25
tacostahern <35584612+tacostahern@users.noreply.github.com>
2019-11-28 16:06:01 -0600

    fixed js slider

--- maps.js
+++ maps.js
@@ -191,31 +191,12 @@ function initMap()
   transitLayer.setMap(map);
 }
 
-var slideIndex = 0;
-showSlides();
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
 
-function showSlides() {
-	var i;
-
-
-	var slides = document.getElementsByClassName("imgfade");
-
-
-	for (i = 0; i < slides.length; i++) {
-
-		slides[i].style.display = "none";
-	}
-
-	slideIndex++;
-
-
-	if (slideIndex > slides.length) {
-		slideIndex = 1;
-	}
-
-
-	slides[slideIndex - 1].style.display = "block";
-
-
-	setTimeout(showSlides, 2000);
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
 }
